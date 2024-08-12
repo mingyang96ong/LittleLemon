1afefb90513408f4dcc9aeb5aa7691a4159e284c
mingyang96ong <mingyang96ong@gmail.com>
2024-08-12 19:10:03 +0800

    Added Authentication with Djoser and Secure Booking API

--- littlelemon/restaurant/urls.py
+++ littlelemon/restaurant/urls.py
@@ -1,7 +1,7 @@
 from django.contrib import admin
 from django.urls import path
 
-from rest_framework.routers import DefaultRouter
+from rest_framework.authtoken.views import obtain_auth_token
 
 from .views import index, MenuItemView, SingleMenuItemView
 
@@ -9,4 +9,5 @@ urlpatterns = [
     path('', index, name='index'),
     path('menu/', MenuItemView.as_view()),
     path('menu/<int:pk>', SingleMenuItemView.as_view()),
+    path('api-token-auth/', obtain_auth_token),
 ]
